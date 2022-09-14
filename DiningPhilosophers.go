1addf48f086110b19c1b8833c52d36d001cc5329
Clara Lyngeraa <clara.lyngeraa@gmail.com>
2022-09-14 12:54:37 +0200

    something is happening

--- DiningPhilosophers.go
+++ DiningPhilosophers.go
@@ -1,44 +1,43 @@
-package main
+// package main
 
-	leftForkAnswer := make(chan string)
-	ph0AswerLeft := make(chan string)
-	rightForkAnswer := make(chan string)
-	Ph0AnswerRight := make(chan string)
+// 	leftForkAnswer := make(chan string)
+// 	ph0AswerLeft := make(chan string)
+// 	rightForkAnswer := make(chan string)
+// 	Ph0AnswerRight := make(chan string)
 
-func main(){
-	go philosopher();
-	go fork();
-	
-}
+// func main(){
+// 	go philosopher();
+// 	go fork();
 
-func philosopher (){
-	for i := 0; i<2; i++{
-		select {
-		case answerLeft  = <- leftForkAnswer:
-			select {
-			 case answerRight  = <- leftForkAnswer:
-				fmt.PrintLn("ph0 eating");
-				ph0AswerLeft <- "i am done eating"
-				ph0AswerRight <- "i am done eating"
+// }
 
-			}
-		}
-	}
-}
+// func philosopher (){
+// 	for i := 0; i<3; i++{
+// 		select {
+// 		case answerLeft  = <- leftForkAnswer:
+// 			select {
+// 			 case answerRight  = <- leftForkAnswer:
+// 				fmt.PrintLn("ph0 eating");
+// 				ph0AswerLeft <- "i am done eating"
+// 				ph0AswerRight <- "i am done eating"
 
-func fork () {
-	var available bool = true;
-	leftForkAnswer <- "I am available"
-	rightForkAnswer <- "I am available"
+// 			}
+// 		}
+// 	}
+// }
 
+// func fork () {
+// 	var available bool = true;
+// 	leftForkAnswer <- "I am available"
+// 	rightForkAnswer <- "I am available"
 
-	select {
-	case request =  <- ph0AswerLeft:
-			available = false;
-			if(available){
-				leftForkAnswer <- "I am available"
-			}	
-		}
-}
+// 	select {
+// 	case request =  <- ph0AswerLeft:
+// 			available = false;
+// 			if(available){
+// 				leftForkAnswer <- "I am available"
+// 			}
+// 		}
+// }
 
 
